0e9e67110c1c7e7424b73b5c8733515674f1928f
leblanc <edwige.leblanc@epfedu.fr>
2025-11-24 22:39:35 +0100

    afficher touches renard

--- Assets/Scripts/FoxRaycastInteract.cs
+++ Assets/Scripts/FoxRaycastInteract.cs
@@ -2,25 +2,51 @@ using UnityEngine;
 
 public class FoxRaycastInteract : MonoBehaviour
 {
-    public FoxPetTrigger fox; // drag & drop ton renard dans l'inspecteur
+    public FoxPetTrigger fox;        // renard
+    public GameObject hintUI;        // texte "T pour caresser"
+    public float interactDistance = 3f;
+
+    void Start()
+    {
+        if (hintUI != null)
+            hintUI.SetActive(false);
+    }
 
     void Update()
     {
+        HandleUI();
+        HandleInput();
+    }
+
+    void HandleUI()
+    {
+        float distance = Vector3.Distance(transform.position, fox.transform.position);
+
+        if (distance <= interactDistance)
+        {
+            if (!hintUI.activeSelf)
+                hintUI.SetActive(true);
+        }
+        else
+        {
+            if (hintUI.activeSelf)
+                hintUI.SetActive(false);
+        }
+    }
+
+    void HandleInput()
+    {
+
         if (Input.GetKeyDown(KeyCode.T))
         {
-            Debug.Log("T"); 
-            if (fox != null)
-            {
-                fox.Pet();
-            }
+            Debug.Log("T : Assis");
+            fox.Pet();
         }
+
         if (Input.GetKeyDown(KeyCode.R))
         {
-            Debug.Log("R"); 
-            if (fox != null)
-            {
-                fox.Roulade();
-            }
+            Debug.Log("R : Jouer");
+            fox.Roulade();
         }
     }
 }
